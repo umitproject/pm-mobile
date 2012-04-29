28e27d9d085aecc67641d1ca7ba69adaa6c6c81e
Gautam Bajaj <gautam1237@gmail.com>
2012-04-29 18:30:22 +0530

    Lastest Code

---
+++ src/com/test/Print.java
@@ -0,0 +1,16 @@
+package com.test;
+
+import java.io.BufferedReader;
+
+import android.app.Activity;
+import android.widget.TextView;
+
+public class Print  {
+//	BufferedReader output;
+	
+	public Print(){
+		
+		System.out.println("Print called");
+		
+	}
+}
---
+++ src/com/test/SniffingService.java
@@ -0,0 +1,20 @@
+package com.test;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+public class SniffingService extends IntentService{
+
+	public SniffingService(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+		
+	}
+
+	@Override
+	protected void onHandleIntent(Intent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
